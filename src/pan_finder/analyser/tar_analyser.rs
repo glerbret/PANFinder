7d9ef53291dc26cfd34350b670f4594571e3fef0
Grégory Lerbret <gregory.lerbret@gmail.com>
2026-05-02 23:39:51 +0200

    Improvements on tar files analyses

--- src/pan_finder/analyser/tar_analyser.rs
+++ src/pan_finder/analyser/tar_analyser.rs
@@ -1,6 +1,7 @@
 use bzip2::read::BzDecoder;
 use flate2::read::GzDecoder;
 use std::fs::File;
+use std::io::Read;
 use tar::{Archive, Entry};
 use walkdir::DirEntry;
 
@@ -11,7 +12,7 @@ use crate::pan_finder::analyser::common::Pattern;
 use crate::pan_finder::analyser::pdf_analyser::analyse_pdf_file_content;
 use crate::pan_finder::analyser::text_analyser::analyse_text_file_content;
 use crate::pan_finder::config::Configuration;
-use crate::pan_finder::utils::{is_pdf_file, is_text_file, read_up_to};
+use crate::pan_finder::utils::{is_pdf_file, is_text_file};
 
 pub fn analyse_tar_file(
     file: &DirEntry,
@@ -148,11 +149,12 @@ fn check_inc_file<T: std::io::Read>(
     config: &Configuration,
     inc_file: &mut Entry<'_, T>,
 ) -> Result<Vec<PanFound>, String> {
-    let size = usize::try_from(inc_file.header().size().unwrap()).unwrap();
-    let mut data = vec![0u8; size];
+    //let size = usize::try_from(inc_file.header().size().unwrap()).unwrap();
+    let mut data = Vec::new();
 
-    match read_up_to(inc_file, &mut data) {
-        Ok(_) => {
+    //    match read_up_to(inc_file, &mut data) {
+    match inc_file.read_to_end(&mut data) {
+        Ok(size) => {
             let filename = inc_file.header().path().unwrap();
             if is_pdf_file(&data, size) {
                 check_pdf_file(patterns_list, config, filename.to_str().unwrap(), data)
@@ -281,6 +283,81 @@ mod tests {
             assert_eq!(
                 res.pan_found_per_subfiles[1].subfilename,
                 "text_present.txt"
+            );
+            assert_eq!(res.pan_found_per_subfiles[1].pan_found.len(), 4);
+            assert_eq!(
+                res.pan_found_per_subfiles[1].pan_found[0].pan,
+                "************0000"
+            );
+            assert_eq!(
+                res.pan_found_per_subfiles[1].pan_found[1].pan,
+                "************0018"
+            );
+            assert_eq!(
+                res.pan_found_per_subfiles[1].pan_found[2].pan,
+                "************0026"
+            );
+            assert_eq!(
+                res.pan_found_per_subfiles[1].pan_found[3].pan,
+                "************0034"
+            );
+        }
+    }
+
+    #[test]
+    fn test_analyse_tar_with_dir() {
+        let patterns = vec![Pattern {
+            brand: String::from("Credit card"),
+            re: Regex::new(r"[2-7]([-\s]*[0-9]{1}){15}").unwrap(),
+            sub_brand: vec![
+                SubBrand {
+                    brand: String::from("BIN 1"),
+                    test_bin: false,
+                    bin_list: vec![String::from("501767")],
+                },
+                SubBrand {
+                    brand: String::from("BIN 2"),
+                    test_bin: false,
+                    bin_list: vec![String::from("507100")],
+                },
+            ],
+        }];
+        let config = Configuration::new();
+
+        for entry in WalkDir::new("testdata/tar_with_dir.tar") {
+            let res = analyse_tar_file(&entry.unwrap(), &patterns, &config).unwrap();
+            assert_eq!(res.pan_found.len(), 0);
+            assert_eq!(res.pan_found_per_subfiles.len(), 2);
+
+            assert_eq!(
+                res.pan_found_per_subfiles[0].subfilename,
+                "tar_with_dir/pdf/pdf_present.pdf"
+            );
+            assert_eq!(res.pan_found_per_subfiles[0].pan_found.len(), 5);
+            assert_eq!(
+                res.pan_found_per_subfiles[0].pan_found[0].pan,
+                "************0000"
+            );
+            assert_eq!(
+                res.pan_found_per_subfiles[0].pan_found[1].pan,
+                "************0018"
+            );
+            assert_eq!(
+                res.pan_found_per_subfiles[0].pan_found[2].pan,
+                "************0026"
+            );
+            assert_eq!(
+                res.pan_found_per_subfiles[0].pan_found[3].pan,
+                "************0034"
+            );
+            assert_eq!(
+                res.pan_found_per_subfiles[0].pan_found[4].pan,
+                "************0042"
+            );
+
+            assert_eq!(
+                res.pan_found_per_subfiles[1].subfilename,
+                "tar_with_dir/txt/text_present.txt"
             );
             assert_eq!(res.pan_found_per_subfiles[1].pan_found.len(), 4);
             assert_eq!(
